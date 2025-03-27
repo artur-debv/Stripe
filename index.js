fdb8c6e938b895a946f02a9a8b21349060f230a3
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2025-03-27 19:35:06 -0300

    instalação do cors para liberar a comunicação do frontend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const cors = require('cors')
 
 const app = express()
 
